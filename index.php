daca4c833c85b6ee4dd7d35dfd2bfe0e8dad7903
Técnico em Informática 2022.2 <36127512022.2@senacrjedu.local>
2022-12-14 16:21:56 -0300

    inicio php

---
+++ index.php
@@ -0,0 +1,18 @@
+  <?php
+
+$num1 = 344.88;
+$num2 = 314.88;
+
+$soma = $num1 + $num2;
+
+
+if($soma >= 200){
+    echo "O valor da soma é maior que R$200";
+} else{
+    echo "O valor da soma é menor que R$200";
+}
+
+ echo"</br> o resultado da soma é R$$soma";
+?>
+
+
